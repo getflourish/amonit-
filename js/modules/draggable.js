7b7b9e2b48bc4978bc16f2dbf93a2a1341326953
Florian Schulz <mail@getflourish.com>
2014-01-23 08:39:14 +0100

    call save after drag & drop

--- js/modules/draggable.js
+++ js/modules/draggable.js
@@ -1,7 +1,11 @@
 angular.module('draggableModule', []).
-directive('draggable', ['$document' , function($document) {
+directive('draggable', ['$document', '$rootScope' , function($document, $rootScope) {
     return {
         restrict: 'A',
+        scope: {
+            annotation: "=a",
+            imageElement: "=image"
+        },
         link: function(scope, elm, attrs) {
             var startX, startY, initialMouseX, initialMouseY;
             var handle = $(elm).find('' + attrs.handle); 
@@ -31,9 +35,9 @@ directive('draggable', ['$document' , function($document) {
                     left: x + 'px',
                 });
 
-                var annotation = scope.current.annotations[attrs.annotationid];
-                annotation.x = x / scope.imageElement.prop("width");
-                annotation.y = y / scope.imageElement.prop("height");
+                
+                scope.annotation.x = x / scope.imageElement.prop("width");
+                scope.annotation.y = y / scope.imageElement.prop("height");
 
                 return false;
             }
@@ -41,7 +45,7 @@ directive('draggable', ['$document' , function($document) {
             function mouseup() {
                 $document.unbind('mousemove', mousemove);
                 $document.unbind('mouseup', mouseup);
-                scope.save();
+                $rootScope.$emit("dragstop");
             }
         }
     };
